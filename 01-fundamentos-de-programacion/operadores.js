ef2da51d9b5e6c74054b75dfedd8c748eebae711
10Montse10 <163039811+10montse10@users.noreply.github.com>
2024-09-23 15:41:54 -0600

    Aprendimos Operadores

---
+++ 01-fundamentos-de-programacion/operadores.js
@@ -0,0 +1,58 @@
+/* 
+  Operadores
+  Son símbolos utilizados para realizar operaciones sobre valores y variables.
+  1. Aritméticos
+  2. Asignación
+  3. Comparación
+*/
+
+/* 
+  1. Operadores Aritméticos
+  Se utilizan para realizar matemáticas.
+
+  */
+// Suma (+): Suma dos valores
+
+let suma = 16 + 6; //22
+console.log(suma);
+
+// Resta (-): Resta el segundo valor el primero
+
+let resta = 64 - 15; //49
+console.log(resta);
+
+// Multiplicación (*): Multiplica los valores
+
+let multi = 15 * 5; //75
+console.log(multi);
+
+//División (/): Divide el primer valor por el segundo
+
+let divi = 80 / 2; //40
+console.log(divi);
+
+// Módulo (%): Devuelve el resto de la división entre dos valores.
+
+let mod = 7 % 2; //1
+let modulus = 9 % 3; //0
+
+console.log(mod);
+console.log(modulus);
+
+//Exponenciación (**) Eleva un número a la potencia del otro.
+
+let expo = 3 ** 2; //9
+
+console.log(expo);
+
+// Incremento (++): Incrementa el valor de una unidad en 1.
+
+let x = 6;
+x++;
+console.log(x);
+
+// Decremento (--): Decrementa el valor en 1.
+
+let y = 8;
+y--;
+console.log(y)
