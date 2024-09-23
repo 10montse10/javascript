a5a2980b39663d3bb467bca3fb6272ae37257a8c
10Montse10 <163039811+10montse10@users.noreply.github.com>
2024-09-23 15:05:46 -0600

    Terminamos de ver Tipos de Datos

--- 01-fundamentos-de-programacion/tipos-de-datos.js
+++ 01-fundamentos-de-programacion/tipos-de-datos.js
@@ -22,11 +22,20 @@
   
   Es el valor que se asigna a una varible que ha sido declara pero no inicializada.
   
-  2. Tipo de Datos Complejos.
+  2. Tipo de Datos Complejos o de Objetos.
   
   * Array = Arreglo
   
   Es un conjunto de datos que los almacena en una lista odenada de elementos. Los datos dentro de los arreglos se escrbiben entre corchetes [] y separados por coma ,
+
+  *Object
+  
+  Es un conjunto de múltiples datos en pares 'clave' 'valor'. Se escreben entre llaves {} y separados por comas ,
+  
+  * Functions o Funciones
+  
+  Es un tipo de datos que puede ser "invocado". Se declaran con la palabra reservada "function" y el código que ejecuta entre llave ["código"]
+
 */
 
 /* Numbers */
@@ -65,10 +74,52 @@ console.log(x); // Valor que el usuario introduzca.
 
 /* Array */
 
-let misVidejuegosFavoritos = ["Subway Surfes, Zig Zag"];
+let misVideojuegosFavoritos = ["Subway Surfes", "Zig Zag"];
 let misNumerosSuerte = [8, 9, 1];
 let otroArray = ["Noble 9", 8, true]
 
-console.log(misVidejuegosFavoritos);
+// Accedemos a los datos de una clave dentro de un objeto.
+
+// Con la notación de []
+
+console.log(misVideojuegosFavoritos);
 console.log(misNumerosSuerte);
 console.log(otroArray);
+
+/* Objeto */
+
+let persona = {
+  
+  // clave : valor,
+  
+  nombre : "Montserrat",
+  edad : 19,
+  colorCabello : "Castaño - obscuro",
+  colorOjos : "Cafes Claro",
+  altura : 1.64,
+  peso : "56 kg",
+  vivo : true,
+  novie : false,
+}
+
+console.log(persona);
+
+// Accedemos a los datos de una clave dentro de un objeto.
+
+// Con la notación de .
+
+console.log(persona.nombre);
+console.log(persona.novie);
+
+/* Function */
+
+let mensajito = function () {
+  
+  // Pasamos el código que ejecuta la función
+  
+  console.log("Este es un mensaje lanzado por una función");
+}
+
+//Invocamos la función
+
+mensajito();
