b549101d12b2ab4cff8995367983e31478be0138
10Montse10 <163039811+10montse10@users.noreply.github.com.>
2024-10-02 12:50:59 -0600

    Práctica

---
+++ 01-fundamentos-de-programacion/practica-condicionales.js
@@ -0,0 +1,12 @@
+let edad = prompt ("¿Qué edad tienes?");
+
+let edad = 20;
+console.log(edad);
+
+if (edad>= 18) {
+    console.log("Eres mayor de edad");
+} else if (edad< 20) {
+    console.log("Eres menor de edad");
+} else {
+    console.log("Eres un alien");
+}
